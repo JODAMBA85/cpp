e123c186c0153835c6f525b1c84b065a04dedc69
Jonatan Amaya <mcruel31@gamil.com>
2022-02-15 20:32:15 -0600

    suma de impares

---
+++ Ejercicio04/Simpares.cpp
@@ -0,0 +1,18 @@
+/* 
+    Proposito:   Programa que sumen, reste, multiplique y divida.
+
+    Ejercicio:   Calculadora
+    Autor:       Jonatan Amaya
+    Universidad: UNAH-VS
+    Carrera:     Licenciatura en Informatica Administrativa
+    Hora:        19:00 - 20:00
+    Catedratico: Master Billy Fernandez
+    Clase:       Lenguaje de Programacion I
+*/ 
+
+
+#include <iostream>
+
+using namespace std;
+
+int main ( int argc, char const *argv[] )
