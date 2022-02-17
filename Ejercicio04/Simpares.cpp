25c03a84775abc253c94d193ab319fb5fc17c54f
Jonatan Amaya <mcruel31@gamil.com>
2022-02-16 21:39:06 -0600

    tarea4

--- Ejercicio04/Simpares.cpp
+++ Ejercicio04/Simpares.cpp
@@ -1,6 +1,5 @@
 /* 
-    Proposito:   Programa que sumen, reste, multiplique y divida.
-
+    Proposito:   Programa que sumen impares y pares.
 
     Ejercicio:   Calculadora
     Autor:       Jonatan Amaya
@@ -13,7 +12,39 @@
 
 
 #include <iostream>
-
 using namespace std;
 
-int main ( int argc, char const *argv[] )
+int main ( int argc, char const *argv[] ){
+    
+    // Datos de entrada
+    int pares = 0;
+    int impares = 0;
+    int Total = 0;
+        
+    // Proceso
+    for(int i= 1; i <= 10; i++)
+
+    {	//declarando que se van a sumar los signos pares
+        if (i % 2 == 0){
+            pares = pares + i;
+
+        }
+        // declaracion de suma de impares
+        else{impares = impares + i;
+
+        }
+        cout << i << "";
+		
+    }
+
+    // sumando pares e impares 
+    Total = pares + impares;
+
+    cout << endl;
+    cout << " total pares es: " << pares << endl;
+    cout << " total impares es:" <<impares << endl;
+    cout << " Total es: " << Total << endl;
+    cout << endl;
+    
+return 0;
+}
