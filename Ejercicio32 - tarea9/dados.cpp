4fba404d7853b856324e15300181b187b000f5c3
Jonatan Amaya <mcruel31@gamil.com>
2022-03-30 21:16:02 -0600

    Tarea9

---
+++ Ejercicio32 - tarea9/dados.cpp
@@ -0,0 +1,43 @@
+/* 
+    Autor:       Jonatan Amaya
+    Universidad: UNAH-VS
+    Carrera:     Licenciatura en Informatica Administrativa
+    Hora:        19:00 - 20:00
+    Catedratico: Master Billy Fernandez
+    Clase:       Lenguaje de Programacion I
+*/ 
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int respuesta=0;
+    int numero=0;
+    srand(time(NULL));
+   do
+   {
+   system("cls");    
+       
+   numero= rand() % 50+1;   cout<< "Primer numero del dado:"<<numero;
+   cout <<endl;
+   
+   numero= rand() % 50+1;   cout<< "segundo numero del dado:"<<numero;
+   cout <<endl;
+
+   cout <<"¿Desea jugar nuevamente? (1.si  2.no):" ;
+   cin>>respuesta;
+   }
+
+   while (respuesta==1); 
+
+   system("cls");
+   cout <<endl;
+   cout <<"¡Gracias por jugar!";
+
+
+    return 0;
+}
